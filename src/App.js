5503230f53da9d0e091d7893060124ecf82761fa
Swedish <tarthurf@yahoo.com>
2020-06-17 12:20:45 -0700

    initial app creation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/main.css'
+
+function App() {
+  return (
+    <div className="font-bold text-red-600">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
