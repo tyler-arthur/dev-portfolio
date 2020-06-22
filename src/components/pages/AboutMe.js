e1fb295fbc5dd3901e98a57b1cf315ffeb7a11f7
Swedish <tarthurf@yahoo.com>
2020-06-21 23:30:56 -0700

    minor design fixes

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -6,56 +6,52 @@ const AboutMe = () => {
   console.log(Me)
 
   return (
-    <React.Fragment>
-      <div className="flex flex-col mx-3 p-6">
-        <div className="text-center space-y-3">
-          <h1 className="text-4xl font-bold text-custom-orange underline">About</h1>
-          
-          <p className="text-lg text-custom-lavender">
-            Hey there! In case you missed it, my name is Tyler. I am a full-stack web developer with expertise in customer service which ensures that my team delivers exactly what the customer wants. 
-          </p>
-          <div className="block h-64 w-64 m-4 rounded-full overflow-hidden">
-            <img className="h-full w-full object-top object-cover" 
-              src={"/static/media/img_1128conv_large_1.1a6a8872.jpg"} 
-              alt="me" 
-            />
-          </div>
-          <p className="text-lg text-custom-lavender">
-            I have a certificate for web development from <span>
-            <a href="https://bootcamp.extension.ucsd.edu/coding/"
-            className="text-custom-aqua underline">UC San Diego Extension</a></span> and 
-            have studied fire sciences at <span>
-            <a href="https://www.sdmiramar.edu/index.php/programs/fire-protection-technology"
-            className="text-custom-aqua underline">SDCCD Miramar</a></span>. 
-            My exposure to many different industries has given me a broad view of client management and building customer relationships.
-          </p>
-          <div className="h-1 w-full rounded-full bg-custom-orange"></div>
-          <h1 className="text-lg text-custom-lavender">Some technologies I like to leverage are:</h1>
-          <div className="flex flex-col justify-around">
-            <div className="flex flex-col space-y-1">
-              <span className="text-custom-orange text-lg"><i className="fab fa-js-square text-2xl text-custom-orange"></i> JavaScript</span>
-              <span className="text-custom-orange text-lg"><i className="fab fa-react text-2xl text-custom-orange"></i> React</span>
-              <span className="text-custom-orange text-lg"><i className="fab fa-envira text-2xl text-custom-orange"></i> MongoDb</span>
-              <span className="text-custom-orange text-lg"><i className="fab fa-node-js text-2xl text-custom-orange"></i> NodeJS</span>
-              <span className="text-custom-orange text-lg"><i className="fas fa-server text-2xl text-custom-orange"></i> Express</span>
-              <span className="text-custom-orange text-lg"><i className="fab fa-css3-alt text-2xl text-custom-orange"></i> Multiple CSS Frameworks</span>
-            </div>
-          </div>
-            <div className="flex justify-around">
-              <Link to="/">
-                <button className="flex-wrap flex-shrink p-2 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg focus:outline-none focus:shadow-outline">
-                  Go Back
-                </button>
-              </Link>
-              <Link to="/projects">
-                <button className="flex-grow flex-shrink p-2 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg focus:outline-none focus:shadow-outline">
-                  Projects
-                </button>
-              </Link>
-            </div>
+    <div className="flex flex-col justify-center mx-3 p-6">
+      <div className="text-center space-y-3">
+        <h1 className="text-4xl font-bold text-custom-orange underline">About</h1>
+        <p className="text-lg text-custom-lavender">
+          Hey there! In case you missed it, my name is Tyler. I am a full-stack web developer with expertise in customer service which ensures that my team delivers exactly what the customer wants. 
+        </p>
+        <div className="h-64 w-64 m-4 rounded-full overflow-hidden">
+          <img className="h-full w-full object-top object-cover" 
+            src={"/static/media/img_1128conv_large_1.1a6a8872.jpg"} 
+            alt="me" 
+          />
         </div>
+        <p className="text-lg text-custom-lavender">
+          I have a certificate for web development from <span>
+          <a href="https://bootcamp.extension.ucsd.edu/coding/"
+          className="text-custom-aqua underline">UC San Diego Extension</a></span> and 
+          have studied fire sciences at <span>
+          <a href="https://www.sdmiramar.edu/index.php/programs/fire-protection-technology"
+          className="text-custom-aqua underline">SDCCD Miramar</a></span>. 
+          My exposure to many different industries has given me a broad view of client management and building customer relationships.
+        </p>
+        <div className="h-1 w-full rounded-full bg-custom-orange"></div>
+        <h1 className="text-lg text-custom-lavender">Some technologies I like to leverage are:</h1>
       </div>
-    </React.Fragment>
+      <div className="flex flex-col text-center space-y-1">
+        <span className="text-custom-orange text-lg"><i className="fab fa-js-square text-2xl text-custom-orange"></i> JavaScript</span>
+        <span className="text-custom-orange text-lg"><i className="fab fa-react text-2xl text-custom-orange"></i> React</span>
+        <span className="text-custom-orange text-lg"><i className="fab fa-envira text-2xl text-custom-orange"></i> MongoDb</span>
+        <span className="text-custom-orange text-lg"><i className="fab fa-node-js text-2xl text-custom-orange"></i> NodeJS</span>
+        <span className="text-custom-orange text-lg"><i className="fas fa-server text-2xl text-custom-orange"></i> Express</span>
+        <span className="text-custom-orange text-lg"><i className="fab fa-css3-alt text-2xl text-custom-orange"></i> Multiple CSS Frameworks</span>
+      </div>
+      <div className="h-1 w-full my-4 rounded-full bg-custom-orange"></div>
+      <div className="flex justify-around">
+        <Link to="/">
+          <button className="flex-wrap flex-shrink p-2 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg focus:outline-none focus:shadow-outline">
+            Go Back
+          </button>
+        </Link>
+        <Link to="/projects">
+          <button className="flex-grow flex-shrink p-2 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg focus:outline-none focus:shadow-outline">
+            Projects
+          </button>
+        </Link>
+      </div>
+    </div>
   );
 }
 
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -20,73 +20,71 @@ const Contact = () => {
   );
 
   return (
-    <div className="flex justify-around mt-10">
-      <div className="flex flex-col items-start justify-center w-2/3 ml-56">
-        <h1 className="w-2/3 text-4xl font-semibold text-custom-orange underline mb-10">
+    <div className="flex flex-col mx-3 p-6">
+      <div className="text-center">
+        <h1 className="text-4xl font-medium text-custom-orange underline">
           What's Next?
         </h1>
-        <p className="w-2/3 self-start text-xl font-medium text-custom-lavender mt-4">
+        <p className="text-xl font-medium text-custom-lavender">
           I am currently looking for work as a developer. If you like what you see, lets get in touch! Contact me, let's get coffee and chat.
         </p>
-        <div className="flex w-2/3">
-          <Link to="/projects">
-            <button className={`w-48 h-12 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg mt-24 mr-16 focus:outline-none focus:shadow-outline`}>
-              Back to projects
-            </button>
-          </Link>
-          <Link to="/">
-            <button className={`w-48 h-12 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg mt-24 focus:outline-none focus:shadow-outline`}>
-              Go back to the start
-            </button>
-          </Link>
-        </div>
       </div>
-        <div className="w-2/3 self-center mt-24">
-          <form className="flex flex-col justify-center items-start "
-            onSubmit={handleSubmit} 
-          >
-            <label className="text-3xl text-custom-orange" for="name">
-              Name
-            </label>
-            <textarea
-              className="w-1/2 text-xl font-semibold rounded-lg text-justify text-center p-2 my-2 focus:outline-none focus:shadow-outline"
-              name="name"
-              type="text"
-              placeholder="What's should I call you?"
-              onChange={handleChange}
-              value={values.name}
-              rows="1"
-              required
-            ></textarea>
-            <label className="text-3xl text-custom-orange" for="email">
-              Email
-            </label>
-            <textarea
-            className="w-1/2 text-xl font-semibold rounded-lg text-justify text-center p-2 my-2 focus:outline-none focus:shadow-outline"
-              name="email"
-              type="email"
-              placeholder="Where can I respond?"
-              onChange={handleChange}
-              value={values.email}
-              rows="1"
-              required
-            ></textarea>
-            <label className="text-3xl text-custom-orange" for="message">
-              Message
-            </label>
-            <textarea
-            className="w-1/2 text-lg rounded-lg text-justify text-center p-2 my-2 focus:outline-none focus:shadow-outline"
-              name="message"
-              type="text"
-              placeholder="What would you like to say?"
-              onChange={handleChange}
-              value={values.message}
-              rows="4"
-              required
-            ></textarea>
-            <button className="w-48 h-12 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg mt-4 focus:outline-none focus:shadow-outline"
-            >Submit</button>
-          </form>
+      <form className="py-5"
+        onSubmit={handleSubmit}>
+        <label className="text-3xl text-custom-orange" htmlFor="name">
+          Name
+        </label>
+        <textarea
+          className=" text-xl font-semibold w-full rounded-lg text-justify text-center p-2 my-2 focus:outline-none focus:shadow-outline"
+          name="name"
+          type="text"
+          placeholder="What's should I call you?"
+          onChange={handleChange}
+          value={values.name}
+          rows="1"
+          required
+        ></textarea>
+        <label className="text-3xl text-custom-orange" htmlFor="email">
+          Email
+        </label>
+        <textarea
+        className=" text-xl font-semibold w-full rounded-lg text-justify text-center p-2 my-2 focus:outline-none focus:shadow-outline"
+          name="email"
+          type="email"
+          placeholder="Where can I respond?"
+          onChange={handleChange}
+          value={values.email}
+          rows="1"
+          required
+        ></textarea>
+        <label className="text-3xl text-custom-orange" htmlFor="message">
+          Message
+        </label>
+        <textarea
+        className=" text-lg w-full rounded-lg text-justify text-center p-2 my-2 focus:outline-none focus:shadow-outline"
+          name="message"
+          type="text"
+          placeholder="What would you like to say?"
+          onChange={handleChange}
+          value={values.message}
+          rows="4"
+          required
+        ></textarea>
+        <button className="flex-wrap flex-shrink p-2 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg focus:outline-none focus:shadow-outline"
+        >Submit</button>
+      </form>
+      <div className="h-1 w-full rounded-full bg-custom-orange"></div>
+      <div className="flex justify-around mt-6">
+        <Link to="/projects">
+          <button className="flex-wrap flex-shrink p-2 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg focus:outline-none focus:shadow-outline">
+            Go Back
+          </button>
+        </Link>
+        <Link to="/">
+          <button className="flex-grow flex-shrink p-2 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg focus:outline-none focus:shadow-outline">
+            Start Over
+          </button>
+        </Link>
       </div>
     </div>
   );
