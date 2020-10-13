4387ee7f0e938c20e5c8201ef9d14de9dd743b41
Swedish <tarthurf@yahoo.com>
2020-10-13 13:23:22 -0700

    added form submission validation on contact.js after form submission, added a button to rerender form after submission

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,21 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useForm from '../../utils/useForm';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 const Contact = () => {
 
+  const [formSubmit, formSubmitState] = useState(false);
+
   const emailSubmit = () => {
-    axios.post("https://formspree.io/xknravez", {values})
-    .then(() => console.log("Form Submitted!"))
-    .catch(err => console.log(err))
+    axios.post("https://formspree.io/xknravez", { values })
+      .then(data => {
+        if (data.status === 200) {
+          formSubmitState(true);
+          console.log("Form Submitted!");
+        }
+      })
+      .catch(err => console.log(err))
   }
 
   const { values, handleChange, handleSubmit } = useForm(
     {
-        name: "",
-        email: "",
-        message: ""
+      name: "",
+      email: "",
+      message: ""
     }, emailSubmit
   );
 
@@ -31,50 +38,67 @@ const Contact = () => {
           I am currently looking for work as a developer. If you like what you see, lets get in touch! Contact me, let's get coffee and chat.
         </p>
       </div>
-      <form className="py-5 md:px-24 lg:px-32 xl:px-40"
-        onSubmit={handleSubmit}>
-        <label className="text-3xl text-custom-orange" htmlFor="name">
-          Name
+      {formSubmit ?
+        <div className="py-8 m-8 flex flex-col space-y-4">
+          <h1 className="text-3xl text-center text-custom-orange">Thanks!</h1>
+          <p className="text-xl md:text-2xl text-center font-medium text-custom-lavender">
+            Your submission was successful! Please allow 48 hours for a response.
+        </p>
+        <button className="self-center flex-grow flex-shrink md:text-2xl p-2 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg focus:outline-none focus:shadow-outline"
+          onClick={() => {
+            values.message = "";
+            formSubmitState(false);
+          }}
+        >
+          Send Another Submission
+        </button>
+        </div>
+        :
+        <form className="py-5 md:px-24 lg:px-32 xl:px-40"
+          onSubmit={handleSubmit}>
+          <label className="text-3xl text-custom-orange" htmlFor="name">
+            Name
         </label>
-        <textarea
-          className=" text-xl font-semibold w-full rounded-lg text-justify text-center p-2 my-2 focus:outline-none focus:shadow-outline"
-          name="name"
-          type="text"
-          placeholder="What should I call you?"
-          onChange={handleChange}
-          value={values.name}
-          rows="1"
-          required
-        ></textarea>
-        <label className="text-3xl text-custom-orange" htmlFor="email">
-          Email
+          <textarea
+            className=" text-xl font-semibold w-full rounded-lg text-Justify p-2 my-2 focus:outline-none focus:shadow-outline"
+            name="name"
+            type="text"
+            placeholder="What should I call you?"
+            onChange={handleChange}
+            value={values.name}
+            rows="1"
+            required
+          ></textarea>
+          <label className="text-3xl text-custom-orange" htmlFor="email">
+            Email
         </label>
-        <textarea
-        className=" text-xl font-semibold w-full rounded-lg text-center p-2 my-2 focus:outline-none focus:shadow-outline"
-          name="email"
-          type="email"
-          placeholder="Where can I respond?"
-          onChange={handleChange}
-          value={values.email}
-          rows="1"
-          required
-        ></textarea>
-        <label className="text-3xl text-custom-orange" htmlFor="message">
-          Message
+          <textarea
+            className=" text-xl font-semibold w-full rounded-lg text-Justify p-2 my-2 focus:outline-none focus:shadow-outline"
+            name="email"
+            type="email"
+            placeholder="Where can I respond?"
+            onChange={handleChange}
+            value={values.email}
+            rows="1"
+            required
+          ></textarea>
+          <label className="text-3xl text-custom-orange" htmlFor="message">
+            Message
         </label>
-        <textarea
-        className=" text-lg w-full rounded-lg  text-center p-2 my-2 focus:outline-none focus:shadow-outline"
-          name="message"
-          type="text"
-          placeholder="What would you like to say?"
-          onChange={handleChange}
-          value={values.message}
-          rows="4"
-          required
-        ></textarea>
-        <button className="flex-wrap flex-shrink md:text-2xl p-2 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg focus:outline-none focus:shadow-outline"
-        >Submit</button>
-      </form>
+          <textarea
+            className=" text-lg w-full rounded-lg  text-Justify p-2 my-2 focus:outline-none focus:shadow-outline"
+            name="message"
+            type="text"
+            placeholder="What would you like to say?"
+            onChange={handleChange}
+            value={values.message}
+            rows="4"
+            required
+          ></textarea>
+          <button className="flex-wrap flex-shrink md:text-2xl p-2 border border-custom-aqua text-custom-aqua rounded-tr-lg rounded-bl-lg focus:outline-none focus:shadow-outline"
+          >Submit</button>
+        </form>
+      }
       <div className="h-1 w-full rounded-full bg-custom-orange"></div>
       <div className="flex justify-around mt-6">
         <Link to="/projects">
